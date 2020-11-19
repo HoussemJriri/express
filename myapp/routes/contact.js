70c7f5676551919f9cc34fbadfab53bbe2e5c879
HoussemJriri <jririhoussem07@gmail.com>
2020-11-19 09:51:31 +0100

    update

--- myapp/routes/contact.js
+++ myapp/routes/contact.js
@@ -1,14 +1,21 @@
-const express = require('express');
+const express = require("express");
 const router = express();
 
-
-router.get('/', /*handleTime,*/ (req, res) => {
+router.get(
+  "/",
+  /*handleTime,*/ (req, res) => {
     var t = new Date();
-    if (9 <= Number(t.getHours()) && Number(t.getHours()) < 17) {
-        res.sendFile(process.cwd() + '\\public\\contact.html')
+    if (
+      9 <= Number(t.getHours()) &&
+      Number(t.getHours()) < 17 &&
+      1 <= Number(t.getDay()) &&
+      Number(t.getDay()) <= 5
+    ) {
+      res.sendFile(process.cwd() + "\\public\\contact.html");
     } else {
-        res.sendFile(process.cwd() + '\\public\\errorpage.html')
-    };
-});
+      res.sendFile(process.cwd() + "\\public\\errorpage.html");
+    }
+  }
+);
 
 module.exports.router = router;
