2d45e7598dcb81f0f553aea0583059254da21f93
HoussemJriri <jririhoussem07@gmail.com>
2020-11-19 10:04:27 +0100

    updating

--- myapp/routes/home.js
+++ myapp/routes/home.js
@@ -1,14 +1,18 @@
-const express = require('express');
+const express = require("express");
 const router = express();
 
-
-router.get('/', (req, res) => {
-    var t = new Date();
-    if (9 <= Number(t.getHours()) && Number(t.getHours()) < 17) {
-        res.sendFile(process.cwd() + '\\public\\home.html')
-    } else {
-        res.sendFile(process.cwd() + '\\public\\errorpage.html')
-    };
+router.get("/", (req, res) => {
+  var t = new Date();
+  if (
+    9 <= Number(t.getHours()) &&
+    Number(t.getHours()) < 17 &&
+    1 <= Number(t.getDay()) &&
+    Number(t.getDay()) <= 5
+  ) {
+    res.sendFile(process.cwd() + "\\public\\home.html");
+  } else {
+    res.sendFile(process.cwd() + "\\public\\errorpage.html");
+  }
 });
 
 module.exports.router = router;
--- myapp/routes/services.js
+++ myapp/routes/services.js
@@ -1,13 +1,17 @@
-const express = require('express');
+const express = require("express");
 const router = express();
 
-
-router.get('/', /*handleTime,*/ (req, res) => {
-    var t = new Date();
-    if (9 <= Number(t.getHours()) && Number(t.getHours()) < 17) {
-        res.sendFile(process.cwd() + '\\public\\services.html')
-    } else {
-        res.sendFile(process.cwd() + '\\public\\errorpage.html')
-    };
+router.get("/", (req, res) => {
+  var t = new Date();
+  if (
+    9 <= Number(t.getHours()) &&
+    Number(t.getHours()) < 17 &&
+    1 <= Number(t.getDay()) &&
+    Number(t.getDay()) <= 5
+  ) {
+    res.sendFile(process.cwd() + "\\public\\services.html");
+  } else {
+    res.sendFile(process.cwd() + "\\public\\errorpage.html");
+  }
 });
 module.exports.router = router;
